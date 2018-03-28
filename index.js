21768a80d64670676e2f55fab7b20ac1ae2f6d6f
Martín Acuña <meacuna@uc.cl>
2018-03-28 16:09:10 -0300

    Major refactor on project structure

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-const https = require('https');
-const querystring = require('querystring');
-
 let config = require('./config.js');
+let Slack = require('./lib/slack.js');
+let Github = require('./lib/github.js');
 
 exports.handler = function(event, context, callback) {
     let response = {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'isBase64Encoded': false
+      'statusCode': 201,
+      'headers': {
+          'Content-Type': 'application/json'
+      },
+      'isBase64Encoded': false
     };
 
-    const body = JSON.parse(event.body);
-    const review = body.review;
-    const numberOfChanges = Object.keys(body.changes || {}).length;
-    if (review.state == 'approved' || (body.action == 'edited' && numberOfChanges == 0)) {
+    let github = new Github();
+    let eventType = github.identifyWebhook(event.headers, JSON.parse(event.body));
+    let data = eventType.data;
+
+    let message = '';
+    switch (eventType.name) {
+      case 'PRRapproved':
+        message = `Te aceptaron tu PR#${data.pullRequest.number} ${data.pullRequest.html_url}.`;
+        break;
+      case 'PRRchangesRequested':
+        message = `Solicitaron cambios en tu PR#${data.pullRequest.number} ${data.pullRequest.html_url}.`;
+        break;
+      case 'PRReditWithChanges':
+        message = `La review de tu PR#${data.pullRequest.number} ${data.pullRequest.html_url} ha sido modificada.`;
+        break;
+      default:
         return callback(null, Object.assign(response, {'statusCode': 204}));
     }
 
-    const pullRequest = body.pull_request;
-    let data = querystring.stringify({
-        token: config.slackBotToken,
-        channel: config.users[pullRequest.user.login],
-        text: `${review.user.login} te dejó una review en tu PR#${pullRequest.number} ${pullRequest.html_url}`
-    });
+    let slack = new Slack(config.slackBotToken);
 
-    const options = {
-      hostname: 'slack.com',
-      port: 443,
-      path: '/api/chat.postMessage',
-      method: 'POST',
-      headers: {
-           'Content-Type': 'application/x-www-form-urlencoded'
-         }
-    };
-    const req = https.request(options, (res) => {
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-          });
+    slack.sendDM(
+     config.users[data.recipient],
+      message,
+      function() {
         return callback(null, Object.assign(response, {'statusCode': 201}));
-    });
-    req.write(data);
-    req.end();
+      }
+    )
 };
